97c6ca7d59c34b2a3fd315b6b423cc015c8bb475
zoczeli <czelizoltandragos@gmail.com>
2026-05-01 00:21:15 +0300

    IST-153: Replace deprecated dynamodb_table with use_lockfile in backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "isteamx-devops-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "isteamx-terraform-lock"
+    bucket       = "isteamx-devops-terraform-state-bucket"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
